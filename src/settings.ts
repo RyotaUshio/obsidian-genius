3f00facbb38320677f0c5ea794d9209336937918
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-10-21 06:57:32 +0900

    add suggester to the template/new note folder path settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-import { PluginSettingTab, Setting } from "obsidian";
+import { AbstractInputSuggest, App, PluginSettingTab, Setting, TAbstractFile, TFile, TFolder } from "obsidian";
 import GeniusPlugin from "./main";
 
 export interface GeniusPluginSettings {
@@ -39,25 +39,30 @@ export class GeniusPluginSettingTab extends PluginSettingTab {
         new Setting(containerEl)
             .setName('Template path')
             .setDesc("")
-            .addText(toggle => toggle
-                .setValue(this.plugin.settings.template ?? '')
-                .onChange(async (value) => {
-                    this.plugin.settings.template = value;
-                    await this.plugin.saveSettings();
-                }));
+            .addText(toggle => {
+                new FileSuggest(this.app, toggle.inputEl);
+                toggle.setValue(this.plugin.settings.template ?? '')
+                    .onChange(async (value) => {
+                        this.plugin.settings.template = value;
+                        await this.plugin.saveSettings();
+                    })
+            });
 
         new Setting(containerEl)
-            .setName('New note path')
+            .setName('New note folder path')
             .setDesc("leave it empty to respect app settings")
-            .addText(toggle => toggle
-                .setValue(this.plugin.settings.folder ?? '')
-                .onChange(async (value) => {
-                    this.plugin.settings.folder = value;
-                    await this.plugin.saveSettings();
-                }));
+            .addText(toggle => {
+                new FolderSuggest(this.app, toggle.inputEl);
+                toggle
+                    .setValue(this.plugin.settings.folder ?? '')
+                    .onChange(async (value) => {
+                        this.plugin.settings.folder = value;
+                        await this.plugin.saveSettings();
+                    })
+            });
 
         new Setting(containerEl)
-            .setName('Delete cache on plugin unload')
+            .setName('Clear cache on plugin unload')
             .setDesc("")
             .addToggle(toggle => toggle
                 .setValue(this.plugin.settings.clearCacheOnUnload)
@@ -66,5 +71,42 @@ export class GeniusPluginSettingTab extends PluginSettingTab {
                     await this.plugin.saveSettings();
                 }));
 
+        new Setting(containerEl)
+            .setName('Link right sidebar to active note')
+            .setDesc("")
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.linkToNote)
+                .onChange(async (value) => {
+                    this.plugin.settings.linkToNote = value;
+                    await this.plugin.saveSettings();
+                }));
+
+    }
+}
+
+
+abstract class AbstractFileSuggest<FileOrFolder extends TAbstractFile> extends AbstractInputSuggest<FileOrFolder> {
+    constructor(app: App, inputEl: HTMLInputElement) {
+        super(app, inputEl);
+    }
+
+    renderSuggestion(file: FileOrFolder, el: HTMLElement): void {
+        el.setText(file.path);
+    }
+
+    selectSuggestion(file: FileOrFolder, evt: MouseEvent | KeyboardEvent): void {
+        this.setValue(file.path);
+    }
+}
+
+class FileSuggest extends AbstractFileSuggest<TFile> {
+    getSuggestions(qeury: string): TFile[] {
+        return this.app.vault.getMarkdownFiles().filter(file => file.path.toLowerCase().includes(qeury.toLowerCase()));
+    }
+}
+
+class FolderSuggest extends AbstractFileSuggest<TFolder> {
+    getSuggestions(qeury: string): TFolder[] {
+        return this.app.vault.getAllLoadedFiles().filter((file): file is TFolder => file instanceof TFolder && file.path.toLowerCase().includes(qeury.toLowerCase()));
     }
 }
